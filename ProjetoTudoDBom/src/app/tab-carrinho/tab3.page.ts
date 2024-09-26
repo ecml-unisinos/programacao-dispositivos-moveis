5b83bd5dfbd32d5a1bd099c30d3dd5a32ce1a065
Elisa Camillo <elisatcamillo@gmail.com>
2024-09-26 20:48:33 -0300

    elemento de aumentar e diminuir quantidade do item

--- ProjetoTudoDBom/src/app/tab-carrinho/tab3.page.ts
+++ ProjetoTudoDBom/src/app/tab-carrinho/tab3.page.ts
@@ -1,14 +1,61 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonInput,
+  IonButton,
+  IonCard,
+  IonCardHeader,
+  IonThumbnail,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonItem,
+  IonRow,
+  IonCol
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton],
+  imports: [
+    IonRow,
+    IonItem,
+    IonCardHeader,
+    IonCard,
+    IonHeader,
+    IonToolbar,
+    IonInput,
+    IonTitle,
+    IonContent,
+    IonThumbnail,
+    ExploreContainerComponent,
+    IonButton,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardContent,
+    FormsModule,
+    IonCol
+  ],
 })
 export class Tab3Page {
   constructor() {}
+
+  quantidade: number = 0;
+
+  mais() {
+    this.quantidade++;
+  }
+
+  menos() {
+    if (this.quantidade > 0) {
+      this.quantidade--;
+    }
+  }
 }
