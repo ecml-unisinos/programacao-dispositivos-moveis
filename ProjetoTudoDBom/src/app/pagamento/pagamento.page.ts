e295c048c662643564f2f12e07456eb344ccb180
Gersontx <gersonteixeira@hotmail.com.br>
2024-09-29 18:28:18 -0300

    Pasta Pagamento VI

--- ProjetoTudoDBom/src/app/pagamento/pagamento.page.ts
+++ ProjetoTudoDBom/src/app/pagamento/pagamento.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-pagamento',
@@ -11,9 +12,6 @@ export class PagamentoPage {
   constructor() { }
 
   finalizarPedido() {
-  
-    // Implementar a lógica de envio do pedido via WhatsApp
-    
     const pedido = `Olá, Pedido finalizado, forma de pagamento: ${this.formaPagamento}`;
     const whatsappLink = `https://api.whatsapp.com/send?phone=5551981783535&text=${encodeURIComponent(pedido)}`;
     window.open(whatsappLink, '_system');
