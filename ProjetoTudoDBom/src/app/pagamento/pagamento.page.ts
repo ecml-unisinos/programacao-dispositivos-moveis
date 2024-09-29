d333d338b221c566d736c60e78592ff4f594dcdc
jessicaceezar <jessicaceezar@gmail.com>
2024-09-29 20:01:43 -0300

    importacao componentes

--- ProjetoTudoDBom/src/app/pagamento/pagamento.page.ts
+++ ProjetoTudoDBom/src/app/pagamento/pagamento.page.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { NgModel } from '@angular/forms';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonList, IonItem, IonLabel, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-pagamento',
   templateUrl: './pagamento.page.html',
   styleUrls: ['./pagamento.page.scss'],
+  standalone: true,
+  imports: [IonLabel, IonItem, IonList, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonSelect, IonSelectOption],
 })
 export class PagamentoPage {
   formaPagamento: string = '';
