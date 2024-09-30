3f0c4231f387671518eabeb5c94f416ce81e5dfb
Gersontx <gersonteixeira@hotmail.com.br>
2024-09-29 22:43:24 -0300

    Correção codigo pagamento.page.spec

--- ProjetoTudoDBom/src/app/pagamento/pagamento.page.spec.ts
+++ ProjetoTudoDBom/src/app/pagamento/pagamento.page.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { expect } from 'chai'; 
+import { expect } from 'chai';
 
 import { PagamentoPage } from './pagamento.page';
 
