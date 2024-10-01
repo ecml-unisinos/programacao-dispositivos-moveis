d334d169deff27b060aa9e2d5b11074959a02eb8
Thiago Birkhan D´Ambrosi <thiagobdambrosi@gmail.com>
2024-09-30 23:35:00 -0300

    contato do whatsapp adicionado

--- ProjetoTudoDBom/src/app/tab-contato/tab2.page.ts
+++ ProjetoTudoDBom/src/app/tab-contato/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonIcon, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
@@ -7,10 +7,15 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonButton, IonIcon, IonGrid, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
 })
 export class Tab2Page {
 
   constructor() {}
 
+  WhatsApp(){
+    const menssagem = 'Olá, vim pelo APP e gostaria de umas informações';
+    const whatsappLink = `https://api.whatsapp.com/send?phone=5551981783535&text=${encodeURIComponent(menssagem)}`;
+    window.open(whatsappLink, '_system');
+  }
 }
